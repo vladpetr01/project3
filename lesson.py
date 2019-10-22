52be13c73f820292c757bbd8a638d483a83ebb15
Vladislav Petrov <vlad_petrov01@mail.ru>
2019-10-22 10:59:09 +0700

    done.

---
+++ lesson.py
@@ -0,0 +1,504 @@
+# The program is designed for students of 2 classes.
+# Training with the multiplication table.
+
+print('Привет! Давай потренируемся в умножении.')
+number = 0
+answer = 0
+while number < 2 or number > 10:
+    number = int(input('Выбери номер от 2 до 10 '))
+if number == 2:
+    while answer != number * 2:
+        answer = int(input('2 * 2 = '))
+        if answer == number * 2:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 3:
+        answer = int(input('2 * 3 = '))
+        if answer == number * 3:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 4:
+        answer = int(input('2 * 4 = '))
+        if answer == number * 4:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 5:
+        answer = int(input('2 * 5 = '))
+        if answer == number * 5:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 6:
+        answer = int(input('2 * 6 = '))
+        if answer == number * 6:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 7:
+        answer = int(input('2 * 7 = '))
+        if answer == number * 7:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 8:
+        answer = int(input('2 * 8 = '))
+        if answer == number * 8:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 9:
+        answer = int(input('2 * 9 = '))
+        if answer == number * 9:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 10:
+        answer = int(input('2 * 10 = '))
+        if answer == number * 10:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+elif number == 3:
+    while answer != number * 2:
+        answer = int(input('3 * 2 = '))
+        if answer == number * 2:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 3:
+        answer = int(input('3 * 3 = '))
+        if answer == number * 3:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 4:
+        answer = int(input('3 * 4 = '))
+        if answer == number * 4:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 5:
+        answer = int(input('3 * 5 = '))
+        if answer == number * 5:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 6:
+        answer = int(input('3 * 6 = '))
+        if answer == number * 6:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 7:
+        answer = int(input('3 * 7 = '))
+        if answer == number * 7:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 8:
+        answer = int(input('3 * 8 = '))
+        if answer == number * 8:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 9:
+        answer = int(input('3 * 9 = '))
+        if answer == number * 9:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 10:
+        answer = int(input('3 * 10 = '))
+        if answer == number * 10:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+elif number == 4:
+    while answer != number * 2:
+        answer = int(input('4 * 2 = '))
+        if answer == number * 2:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 3:
+        answer = int(input('4 * 3 = '))
+        if answer == number * 3:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 4:
+        answer = int(input('4 * 4 = '))
+        if answer == number * 4:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 5:
+        answer = int(input('4 * 5 = '))
+        if answer == number * 5:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 6:
+        answer = int(input('4 * 6 = '))
+        if answer == number * 6:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 7:
+        answer = int(input('4 * 7 = '))
+        if answer == number * 7:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 8:
+        answer = int(input('4 * 8 = '))
+        if answer == number * 8:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 9:
+        answer = int(input('4 * 9 = '))
+        if answer == number * 9:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 10:
+        answer = int(input('4 * 10 = '))
+        if answer == number * 10:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+elif number == 5:
+    while answer != number * 2:
+        answer = int(input('5 * 2 = '))
+        if answer == number * 2:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 3:
+        answer = int(input('5 * 3 = '))
+        if answer == number * 3:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 4:
+        answer = int(input('5 * 4 = '))
+        if answer == number * 4:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 5:
+        answer = int(input('5 * 5 = '))
+        if answer == number * 5:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 6:
+        answer = int(input('5 * 6 = '))
+        if answer == number * 6:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 7:
+        answer = int(input('5 * 7 = '))
+        if answer == number * 7:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 8:
+        answer = int(input('5 * 8 = '))
+        if answer == number * 8:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 9:
+        answer = int(input('5 * 9 = '))
+        if answer == number * 9:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 10:
+        answer = int(input('5 * 10 = '))
+        if answer == number * 10:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+elif number == 6:
+    while answer != number * 2:
+        answer = int(input('6 * 2 = '))
+        if answer == number * 2:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 3:
+        answer = int(input('6 * 3 = '))
+        if answer == number * 3:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 4:
+        answer = int(input('6 * 4 = '))
+        if answer == number * 4:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 5:
+        answer = int(input('6 * 5 = '))
+        if answer == number * 5:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 6:
+        answer = int(input('6 * 6 = '))
+        if answer == number * 6:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 7:
+        answer = int(input('6 * 7 = '))
+        if answer == number * 7:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 8:
+        answer = int(input('6 * 8 = '))
+        if answer == number * 8:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 9:
+        answer = int(input('6 * 9 = '))
+        if answer == number * 9:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 10:
+        answer = int(input('6 * 10 = '))
+        if answer == number * 10:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+elif number == 7:
+    while answer != number * 2:
+        answer = int(input('7 * 2 = '))
+        if answer == number * 2:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 3:
+        answer = int(input('7 * 3 = '))
+        if answer == number * 3:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 4:
+        answer = int(input('7 * 4 = '))
+        if answer == number * 4:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 5:
+        answer = int(input('7 * 5 = '))
+        if answer == number * 5:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 6:
+        answer = int(input('7 * 6 = '))
+        if answer == number * 6:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 7:
+        answer = int(input('7 * 7 = '))
+        if answer == number * 7:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 8:
+        answer = int(input('7 * 8 = '))
+        if answer == number * 8:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 9:
+        answer = int(input('7 * 9 = '))
+        if answer == number * 9:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 10:
+        answer = int(input('7 * 10 = '))
+        if answer == number * 10:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+elif number == 8:
+    while answer != number * 2:
+        answer = int(input('8 * 2 = '))
+        if answer == number * 2:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 3:
+        answer = int(input('8 * 3 = '))
+        if answer == number * 3:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 4:
+        answer = int(input('8 * 4 = '))
+        if answer == number * 4:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 5:
+        answer = int(input('8 * 5 = '))
+        if answer == number * 5:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 6:
+        answer = int(input('8 * 6 = '))
+        if answer == number * 6:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 7:
+        answer = int(input('8 * 7 = '))
+        if answer == number * 7:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 8:
+        answer = int(input('8 * 8 = '))
+        if answer == number * 8:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попрбуй еще раз')
+    while answer != number * 9:
+        answer = int(input('8 * 9 = '))
+        if answer == number * 9:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 10:
+        answer = int(input('8 * 10 = '))
+        if answer == number * 10:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+elif number == 9:
+    while answer != number * 2:
+        answer = int(input('9 * 2 = '))
+        if answer == number * 2:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 3:
+        answer = int(input('9 * 3 = '))
+        if answer == number * 3:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 4:
+        answer = int(input('9 * 4 = '))
+        if answer == number * 4:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 5:
+        answer = int(input('9 * 5 = '))
+        if answer == number * 5:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 6:
+        answer = int(input('9 * 6 = '))
+        if answer == number * 6:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 7:
+        answer = int(input('9 * 7 = '))
+        if answer == number * 7:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 8:
+        answer = int(input('9 * 8 = '))
+        if answer == number * 8:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 9:
+        answer = int(input('9 * 9 = '))
+        if answer == number * 9:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 10:
+        answer = int(input('9 * 10 = '))
+        if answer == number * 10:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+elif number == 10:
+    while answer != number * 2:
+        answer = int(input('10 * 2 = '))
+        if answer == number * 2:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 3:
+        answer = int(input('10 * 3 = '))
+        if answer == number * 3:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 4:
+        answer = int(input('10 * 4 = '))
+        if answer == number * 4:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 5:
+        answer = int(input('10 * 5 = '))
+        if answer == number * 5:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 6:
+        answer = int(input('10 * 6 = '))
+        if answer == number * 6:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 7:
+        answer = int(input('10 * 7 = '))
+        if answer == number * 7:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 8:
+        answer = int(input('10 * 8 = '))
+        if answer == number * 8:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 9:
+        answer = int(input('10 * 9 = '))
+        if answer == number * 9:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+    while answer != number * 10:
+        answer = int(input('10 * 10 = '))
+        if answer == number * 10:
+            print('Молодец! Продолжаем')
+            break
+        print('Неверно. Попробуй еще раз')
+print('Молодец! Ты всё решил. Пока!')
